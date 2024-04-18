63d66a0767da7e7a89e672f4a2620510c96f8b74
KAkos27 <akoskosztolanyi@gmail.com>
2024-04-18 10:45:13 +0200

    classok

---
+++ src/main/Ember.java
@@ -0,0 +1,12 @@
+package main;
+
+public class Ember {
+
+    String nev;
+    int kor;
+    boolean nem;
+
+    void megszolal() {
+        System.out.println("Szia");
+    }
+}
---
+++ src/main/OOP.java
@@ -0,0 +1,15 @@
+package main;
+
+public class OOP {
+
+    public static void main(String[] args) {
+        Ember gergo = new Ember();
+
+        gergo.nev = "Gergő";
+        gergo.kor = 20;
+        gergo.nem = false;
+
+        gergo.megszolal();
+    }
+
+}
